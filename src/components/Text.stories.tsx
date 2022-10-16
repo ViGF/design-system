538b031526b1f2b07d5dda34f9b8b50bdc2556cc
ViGF <vitoriogdef@gmail.com>
2022-10-16 15:06:14 -0300

    Initial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from "./Text"
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem Ipsum',
+        size: 'md'
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Testing with p</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        },
+        size: {
+            control: {
+                type: 'inline-radio'
+            }
+        },
+        asChild: {
+            table: {
+                disable: true
+            }
+        }
+    }
+}
